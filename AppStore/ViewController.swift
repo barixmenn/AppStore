be7470d04ed1890f0a8bde3e16d73003b08d4a4b
baris dilekci <bdilekci200@gmail.com>
2023-04-24 19:12:14 +0300

    Initial Commit

---
+++ AppStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by Baris on 24.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
